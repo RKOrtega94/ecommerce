89e3d281e7ff1198761a6ed92635bb3dee52ae0b
RKOrtega94 <rkortega1994@gmail.com>
2025-06-29 13:56:13 -0500

    Update submodule references and enhance database creation script

--- create-databases.sql
+++ create-databases.sql
@@ -1,4 +1,18 @@
 -- Create security database
 CREATE DATABASE security
-  WITH OWNER = 'postgres'
-       ENCODING = 'UTF8';
+WITH
+    OWNER = 'postgres' ENCODING = 'UTF8';
+
+DO $$ BEGIN IF NOT EXISTS (
+    SELECT
+    FROM pg_database
+    WHERE
+        datname = 'core'
+) THEN PERFORM dblink_exec (
+    'dbname=' || current_database (),
+    'CREATE DATABASE core WITH OWNER = ''postgres'' ENCODING = ''UTF8'''
+);
+
+END IF;
+
+END $$ LANGUAGE plpgsql;
