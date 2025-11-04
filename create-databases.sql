cfdea43088d3bee21633528ceccbb82d83b48a78
RKOrtega94 <44837907+RKOrtega94@users.noreply.github.com>
2025-11-04 18:55:06 -0500

    Refactor database configurations and update Docker Compose for new auth service

--- create-databases.sql
+++ create-databases.sql
@@ -1,6 +1,17 @@
 -- Create security database
 CREATE DATABASE security
 WITH
-    OWNER = 'postgres' ENCODING = 'UTF8';
+    OWNER = postgres
+    ENCODING = 'UTF8';
 
-CREATE DATABASE core WITH OWNER = 'postgres' ENCODING = 'UTF8';
+-- Create core database
+CREATE DATABASE core
+WITH
+    OWNER = postgres
+    ENCODING = 'UTF8';
+
+-- Create auth_db database
+CREATE DATABASE auth_db
+WITH
+    OWNER = postgres
+    ENCODING = 'UTF8';
