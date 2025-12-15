d9d43c407338e6e585b6e8672d0e80c12d0f65f5
RKOrtega94 <44837907+RKOrtega94@users.noreply.github.com>
2025-12-14 22:51:19 -0500

    feat: enhance Dockerfile and compose setup for local development with hot reload support

--- backend/initdb.sql
+++ backend/initdb.sql
@@ -1,7 +1,7 @@
 -- Database owner configuration
 CREATE DATABASE auth_db WITH OWNER=postgres ENCODING='UTF8';
 CREATE DATABASE security_db WITH OWNER=postgres ENCODING='UTF8';
-CREATE DATABASE globals WITH OWNER=postgres ENCODING='UTF8';
+CREATE DATABASE global_service_db WITH OWNER=postgres ENCODING='UTF8';
 CREATE DATABASE invoicing WITH OWNER=postgres ENCODING='UTF8';
 CREATE DATABASE electronic_signature_db WITH OWNER=postgres ENCODING='UTF8';
 CREATE DATABASE gateway_db WITH OWNER=postgres ENCODING='UTF8';
